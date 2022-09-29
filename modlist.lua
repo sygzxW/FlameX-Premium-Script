082b37f27888f1d84ca05a50dad0a5a1d3d8e58c
sygzxW <73584462+sygzxW@users.noreply.github.com>
2022-09-29 12:08:14 +0200

    Update and rename EzSon to modlist.lua

---
+++ modlist.lua
@@ -0,0 +1,27 @@
+MODS = {
+    [1423817372] = true; -- zynanox
+}
+
+ADMIN = {
+    [0] = true; -- zynanox
+}
+
+function initiateNames()
+    game.Players.LocalPlayer.Character.UpperTorso:FindFirstChild('OriginalSize'):Destroy()
+    for _,v in pairs(game:GetService('Players'):GetPlayers()) do
+        if v.Character then
+            if not v.Character.UpperTorso:FindFirstChild('OriginalSize') then
+                v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[😎] ' .. v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName)
+            end
+            if MODS[v.UserId] then
+                v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[⭐] ' .. v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName)
+            end
+            if ADMIN[v.UserId] then
+                v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[👑] ' .. v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName)
+            end
+        end
+    end
+end
+
+local successful, errored = pcall(initiateNames)
+return MODS
